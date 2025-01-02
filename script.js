6de0830aa5c1138c394e58944d486fb0ef22287c
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-01-02 15:37:26 +0600

    change design add backend

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ closeBtn.addEventListener('click', function (event) {
 sideBar.addEventListener('click', function (event) {
     event.stopPropagation();
 });
+
+// Select both .cards elements
+// const cards = document.querySelectorAll('.cards');
+
+// if (cards.length === 2) {
+//     const [cards1, cards2] = cards; // Assuming there are exactly two cards
+
+//     // Function to synchronize scroll positions
+//     const syncScroll = (source, target) => {
+//         target.scrollLeft = source.scrollLeft;
+//     };
+
+//     // Add event listeners to both elements
+//     cards1.addEventListener('scroll', () => syncScroll(cards1, cards2));
+//     cards2.addEventListener('scroll', () => syncScroll(cards2, cards1));
+// }
+
+
+
+
+
+
+
